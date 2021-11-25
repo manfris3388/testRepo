04aaf31835b3ec3694b19bd7dbbf1b006221fa3d
manfris3388 <lmanfredas@gmail.com>
2021-11-25 19:16:09 +0200

    added goodbye

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbey");
         }
     }
 }
