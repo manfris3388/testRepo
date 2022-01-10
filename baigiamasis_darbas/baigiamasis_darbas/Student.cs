aa24fdfd26cb327fa6db8bf5936c03d0da64c174
manfris3388 <lmanfredas@gmail.com>
2022-01-10 08:13:45 +0200

    final_work
    Initial work

---
+++ baigiamasis_darbas/baigiamasis_darbas/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baigiamasis_darbas
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int DiaryId { get; set; }
+
+        public Student(int id, string name, string surname, int diaryId)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            DiaryId = diaryId;
+        }
+    }
+}
